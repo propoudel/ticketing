d2691f273df54b6db6b295602e2423d3769c2e9f
Tuklal Poudel <tuklal@pegotec.net>
2020-10-01 13:17:04 +0545

    test

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,8 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      //baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: '/',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      //baseURL: 'bazaarbro.com',
       headers: req.headers
     });
   } else {
